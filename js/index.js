6073a8593ca7a1ef283535a8d861de7556262bc2
Agustin Sau <sau.agustin.i@gmail.com>
2025-06-05 14:52:29 -0300

    menu hamburguesa animado

--- js/index.js
+++ js/index.js
@@ -53,3 +53,25 @@ Lo convierte en un archivo .json.
 
 Lo descarga automáticamente como actividades.json.
 */
+
+
+/* menu desplegable hamburguesa */
+
+const btnMenu = document.getElementById('btnMenu');
+const menu = document.getElementById('menuDesplegable');
+
+// mostrar o esconder menu al apretar en el icono
+btnMenu.addEventListener('click', (e) => {
+    e.preventDefault();
+    menu.classList.toggle('mostrar');
+});
+
+// esconde el menu si se apreta afuera del menu
+document.addEventListener('click', (e) => {
+    const clickeaDentro = menu.contains(e.target);
+    const clickeaBoton = btnMenu.contains(e.target);
+
+    if (!clickeaDentro && !clickeaBoton) {
+        menu.classList.remove('mostrar');
+    }
+});
