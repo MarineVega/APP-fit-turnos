5fab448096cf630a712c8ed665fd84c9a718b1cb
Agustin Sau <sau.agustin.i@gmail.com>
2025-06-06 14:07:22 -0300

    pagina turnos html, css y js

---
+++ js/turnos.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const tarjetas = document.querySelectorAll(".actividad-card");
+
+  tarjetas.forEach((tarjeta) => {
+    tarjeta.addEventListener("click", () => {
+    
+      tarjetas.forEach((t) => t.classList.remove("seleccionada"));
+      tarjeta.classList.add("seleccionada");
+      
+    });
+  });
+});
