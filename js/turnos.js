6d60767d51e047a03c462dcfbe61da3573623592
Agustin Sau <sau.agustin.i@gmail.com>
2025-06-11 19:56:49 -0300

    footer html y css

--- js/turnos.js
+++ js/turnos.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const tarjetas = document.querySelectorAll(".actividad-card");
 
   tarjetas.forEach((tarjeta) => {
