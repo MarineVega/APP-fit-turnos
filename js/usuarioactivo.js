1d844a2ffe3ca8adaf8c920bcf7e4410b2c0d84a
nataliagelmi <nataliagelmi@gmail.com>
2025-06-23 13:25:36 -0300

    se cambia los ids acorde al menú

--- js/usuarioactivo.js
+++ js/usuarioactivo.js
@@ -1,63 +1,64 @@
 document.addEventListener("DOMContentLoaded", function () {
   const nombreUsuario = document.getElementById("nombreUsuario");
-  const btnCerrarSesion = document.getElementById("btnCerrarSesion");
+  const menuCerrarSesion = document.getElementById("menuCerrarSesion");
   const userBar = document.getElementById("userBar");
 
-  const btnIniciarSesion = document.getElementById("btnIniciarSesion");
-  const btnCrearCuenta = document.getElementById("btnCrearCuenta");
-  const btnAdmin = document.getElementById("btnAdmin");
+  const menuIniciarSesion = document.getElementById("menuIniciarSesion");
+  const menuCrearCuenta = document.getElementById("menuCrearCuenta");
+  const menuAdmin = document.getElementById("menuAdmin");
 
   const usuarioActivo = JSON.parse(localStorage.getItem("usuarioActivo"));
+
   // si el usuario esta activo, pone el nombre en la barra de navegacion si existe
   if (usuarioActivo) {
     if (nombreUsuario && userBar) {
       userBar.style.display = "flex";
 
-      // si el usuario es administrador  debe mostrarle en el menu la opcion administrar
+      // si el usuario es administrador debe mostrarle en el menu la opcion administrar
       if (usuarioActivo.esAdmin) {
         nombreUsuario.textContent = usuarioActivo.nombre + " (Admin)";
         // se supone que el elemento existe pero se hace para evitar errores.    
-        if (btnAdmin) {
-          btnAdmin.style.display = "block";
+        if (menuAdmin) {
+          menuAdmin.style.display = "block";
         }
       } else {
         nombreUsuario.textContent = usuarioActivo.nombre;
 
-        if (btnAdmin) {
-          btnAdmin.style.display = "none";
+        if (menuAdmin) {
+          menuAdmin.style.display = "none";
         }
       }
     }
 
-    // es buena practica hacerlo, preguntar por el elemento antes de manipularlo
-
-    if (btnIniciarSesion) {
-      btnIniciarSesion.style.display = "none";
+    // es buena practica preguntar si existe el elemento antes de manipularlo
+    if (menuIniciarSesion) {
+      menuIniciarSesion.style.display = "none";
     }
 
-    if (btnCrearCuenta) {
-      btnCrearCuenta.style.display = "none";
+    if (menuCrearCuenta) {
+      menuCrearCuenta.style.display = "none";
     }
   } else {
-    if (btnIniciarSesion) {
-      btnIniciarSesion.style.display = "block";
+    if (menuIniciarSesion) {
+      menuIniciarSesion.style.display = "block";
     }
 
-    if (btnCrearCuenta) {
-      btnCrearCuenta.style.display = "block";
+    if (menuCrearCuenta) {
+      menuCrearCuenta.style.display = "block";
     }
 
-    if (btnAdmin) {
-      btnAdmin.style.display = "none";
+    if (menuAdmin) {
+      menuAdmin.style.display = "none";
     }
 
     if (userBar) {
       userBar.style.display = "none";
     }
   }
- // aca consulta si desea cerrar la sesion si es Si borra el usuario activo y vuelve a index
-  if (btnCerrarSesion) {
-    btnCerrarSesion.addEventListener("click", function (e) {
+
+  // aca consulta si desea cerrar la sesion si es Sí borra el usuario activo y vuelve a index
+  if (menuCerrarSesion) {
+    menuCerrarSesion.addEventListener("click", function (e) {
       e.preventDefault();
 
       Swal.fire({
